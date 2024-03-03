e03da0314f3a4c3ccaba4887c676956fb7d5be2c
Pierre Bellon <bellon.pierre@gmail.com>
2024-03-03 23:46:54 +0100

    Rewrite benchmark script

--- scripts/benchmark/benchmark.sh
+++ scripts/benchmark/benchmark.sh
@@ -3,82 +3,87 @@
 current_time=$(date "+%Y%m%d-%H%M%S")
 script_dir=$(dirname "$0")
 report="$script_dir/out/report-$current_time.md"
-lines_report="$script_dir/out/lines.md"
-words_report="$script_dir/out/words.md"
-bytes_report="$script_dir/out/bytes.md"
-wildcard_report="$script_dir/out/wildcard.md"
 warmup=100
-runs=700
+runs=400
 
 cargo build --release
 
-local-wc(){
+# Function to get the path to the local wc command
+local_wc() {
     realpath "./target/release/wc"
 }
 
-# hyperfine wrapper
+# Function to run hyperfine benchmarks
 hf() {
     hyperfine --warmup "$warmup" --runs "$runs" "${@}"
 }
 
-# expects 2 arg:
-# [1] => export file (markdown)
-# [2] => mode -w | -l | --words | --lines
-# construct args list based on data folder to avoid redundancy
-bench-progressive(){
-    export_md=$1
-    mode=$2
-
-    args=("--export-markdown" "$export_md")
-    for data_fn in "$script_dir"/data/*.txt
-    do
-        data_fn_relative=$(realpath "$data_fn" --relative-to="$script_dir")
-        cmd="$(local-wc) $data_fn $mode"
-        args+=("-n" "wc $data_fn_relative $mode" "$cmd")
-    done
-
-    hf "${args[@]}"
-
-    echo "## \`wc $mode\` benchmark" >> "$report"
-    cat "$export_md" >> "$report"
-    rm "$export_md"
+append_to_report() {
+    echo $1 >>"$report"
 }
 
-get-data(){
-    files=()
-    for f in "$script_dir"/data/*.txt
-    do
-        files+=("$f")
-    done
-
-    echo "${files[@]}"
+# Function to append results to the report and clean up
+append_results_to_report_and_cleanup() {
+    append_to_report "## \`wc$2\` benchmark"
+    cat "$1" >>"$report"
+    rm "$1"
 }
 
-bench-wildcard(){
-    # evaluate data/*.txt wildcard to get all files
-    # data_files=("$script_dir"/data/*.txt)
+# Function to get data files
+get_data_files() {
+    echo "$script_dir"/data/*.txt
+}
 
-    wc_path=$(local-wc)
+# Benchmarks for lines, words, bytes, and all
+bench_modes() {
+    bench_modes=("lines" "words" "bytes" "all")
+    for mode in "${bench_modes[@]}"; do
+        mode_report="$script_dir/out/${mode}.md"
+        export_md="--export-markdown $mode_report"
 
-    local_cmd="$wc_path $(get-data)"
-    os_cmd="wc $(get-data)"
+        # Check if the mode is "all"
+        if [ "$mode" == "all" ]; then
+            mode_arg=""
+        else
+            mode_arg=" --${mode}"
+        fi
+
+        args=($export_md)
+        for data_fn in $(get_data_files); do
+            data_fn_relative=$(realpath "$data_fn" --relative-to="$script_dir")
+            cmd="$(local_wc) $data_fn$mode_arg"
+            args+=("-n" "wc $data_fn_relative$mode_arg" "$cmd")
+        done
+
+        hf "${args[@]}"
+        append_results_to_report_and_cleanup "$mode_report" "$mode_arg"
+    done
+}
+
+# Benchmark for wildcard
+bench_wildcard() {
+    wildcard_report="$script_dir/out/wildcard.md"
+    wc_path=$(local_wc)
+    data_files=$(get_data_files)
+
+    local_cmd="$wc_path $data_files"
+    os_cmd="wc $data_files"
 
     hf --export-markdown "$wildcard_report" \
         -n "wc(local) data/*.txt" "$local_cmd" \
         -n "wc(GNU) data/*.txt" "$os_cmd"
 
-    echo "## \`wc data/*.txt\` benchmark"  >> "$report"
-    cat "$wildcard_report" >> "$report"
-    rm "$wildcard_report"
+    append_results_to_report_and_cleanup "$wildcard_report" " data/*.txt"
 }
 
+touch "$report"
 
-echo "# Benchmark report" > "$report"
+append_to_report "# Benchmark report"
+append_to_report "## Settings"
+append_to_report "| name | value |"
+append_to_report "|------|-------|"
+append_to_report "| **Number of runs** | $runs |"
+append_to_report "| **Number of warumps**| $warmup |"
 
-bench-wildcard
-
-bench-progressive "$lines_report" --lines
-bench-progressive "$words_report" --words
-bench-progressive "$bytes_report" --bytes
-
-
+bench_wildcard
+bench_modes
